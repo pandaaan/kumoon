8ee65c0d7929baa58ad9f896fa6ec48d7bddd5ef
pandaaan <nhudan06@gmail.com>
2020-11-06 22:10:55 +0100

    add title

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import WbCloudyIcon from '@material-ui/icons/WbCloudy';
 import './App.scss';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Logo from './images/may_logo.svg';
 import Prolog from './Components/Prolog/Prolog';
 import ChapterOne from './Components/ChapterOne/ChapterOne';
 
 class App extends Component {
   componentDidMount() {
     AOS.init({
-      duration: 2000
+      duration: 2000,
+      disable: 'mobile'
     });
   }
   render() {
     return (
       <Container className="bg-main">
-        <Row>
-          <Col>
-            <h4>mây <WbCloudyIcon /></h4>
+        <Row className="title-bg align-items-center justify-content-center">
+          <Col xs={12} className="d-flex justify-content-center">
+            <div className="" data-aos="fade-up text-center">
+              <img src={Logo} className="logo"></img>
+              <h2>let's get creative.</h2>
+              <br />
+              <p className="subtitle">11/20</p>
+              <p>dan, ly, my</p>
+            </div>
+
           </Col>
         </Row>
 
@@ -27,7 +35,7 @@ class App extends Component {
         <div className="place-empty"></div>
         <ChapterOne />
         <div className="place"></div>
-      </Container>
+      </Container >
     );
   }
 }
