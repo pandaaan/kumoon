6e33ff1b8d1ff912c6f3a3297ee6a02a0ecb7bee
pandaaan <nhudan06@gmail.com>
2020-11-06 22:14:14 +0100

    change subtitle font size

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
           <Col xs={12} className="d-flex justify-content-center">
             <div className="" data-aos="fade-up text-center">
               <img src={Logo} className="logo"></img>
-              <h2>let's get creative.</h2>
+              <h2 className="subtitle-1">let's get creative.</h2>
               <br />
               <p className="subtitle">11/20</p>
               <p>dan, ly, my</p>
