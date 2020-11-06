1ea9033013801e137e969d6977ceee2a63adcaf8
pandaaan <nhudan06@gmail.com>
2020-11-06 20:55:13 +0100

    Updates

---
+++ precache-manifest.effb44008a2cc703166b1e371da70484.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7d28837ee81d2f2f0e875cb1af850e",
+    "url": "/kumoon/index.html"
+  },
+  {
+    "revision": "934e44ce897d3826acb8",
+    "url": "/kumoon/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "77d73efca3a1de07f579",
+    "url": "/kumoon/static/css/main.10e297d7.chunk.css"
+  },
+  {
+    "revision": "934e44ce897d3826acb8",
+    "url": "/kumoon/static/js/2.68d4c206.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/kumoon/static/js/2.68d4c206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d73efca3a1de07f579",
+    "url": "/kumoon/static/js/main.c915f1b1.chunk.js"
+  },
+  {
+    "revision": "e8ff1e685f67e1b8a61a",
+    "url": "/kumoon/static/js/runtime-main.6fcd8d15.js"
+  },
+  {
+    "revision": "9c92368a783f99b7b411bcef5102bd54",
+    "url": "/kumoon/static/media/brainstorming.9c92368a.svg"
+  },
+  {
+    "revision": "f56497074b0f542fe022364c4802d771",
+    "url": "/kumoon/static/media/radio-waves.f5649707.svg"
+  },
+  {
+    "revision": "f4d7ec09153edbbb8a750577cb94daea",
+    "url": "/kumoon/static/media/whatsapp.f4d7ec09.svg"
+  }
+]);
